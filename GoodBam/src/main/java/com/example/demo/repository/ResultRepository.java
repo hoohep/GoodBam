4b4f0e3807c92eeb08e41b08d78ca934052e89fe
JaYeon <tjwkdus3514@gmail.com>
2024-08-23 11:12:53 +0900

    서자연 - result 변경

--- GoodBam/src/main/java/com/example/demo/repository/ResultRepository.java
+++ GoodBam/src/main/java/com/example/demo/repository/ResultRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repository;
 
-import java.sql.Timestamp;
-import java.util.Optional;
+import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +9,10 @@ import com.example.demo.model.Result;
 
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long>{
-	Optional<Result> findByRUUidAndRCredate(String r_u_uid, Timestamp r_credate);
 	
-	boolean existsByRUUidAndRCredate(String r_u_uid, Timestamp r_credate);
+	Result findByEmailAndRdate(String email, LocalDate rdate);
+
+    boolean existsByEmailAndRdate(String email, LocalDate rdate);
 	
 	
 }
