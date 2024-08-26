133bd80b9d9ed2b102e7060c52878c81a88e2a2e
JaYeon <tjwkdus3514@gmail.com>
2024-08-26 11:53:25 +0900

    서자연 - list 추가

--- GoodBam/src/main/java/com/example/demo/repository/ResultRepository.java
+++ GoodBam/src/main/java/com/example/demo/repository/ResultRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,9 +11,15 @@ import com.example.demo.model.Result;
 @Repository
 public interface ResultRepository extends JpaRepository<Result, Long>{
 	
+	// 수면분석결과 데이터 찾
 	Result findByEmailAndRdate(String email, LocalDate rdate);
-
     boolean existsByEmailAndRdate(String email, LocalDate rdate);
 	
+    // 수면분석결과 리스트 
+	List<Result> findByEmail(String email);
+	boolean existsByEmail(String email);
 	
+	
+    
+    
 }
