e7ef02d5b0a573ac1de14b8781c83e4663a76738
JaYeon <tjwkdus3514@gmail.com>
2024-08-27 14:42:02 +0900

    서자연 - result 수정

--- GoodBam/src/main/java/com/example/demo/controller/ResultRestController.java
+++ GoodBam/src/main/java/com/example/demo/controller/ResultRestController.java
@@ -28,25 +28,28 @@ public class ResultRestController {
 	@Autowired
 	private ResultService service;
 
-	// 수면 분석 결과
+	// 수면 분석 결과 요청
 	@GetMapping("/result")
 	public ResponseEntity<Result> result(HttpServletRequest request) {
+		// react에서 Header에 실어서 요청한 값 가져오기   
 		String authorizationHeader = request.getHeader("Authorization");
 
 		String email = null;
 		LocalDate currentDate = null;
 
+		// 넘어 온 값에서 토큰 처리하기
 		if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			// 토큰 값 앞에 "Bearer" 지우기
 			String token = authorizationHeader.substring(7);
-			// token에서 email값만 파싱
+			// 넘어온 token에서 email값만 파싱하기
 			email = jwtUtil.getUserId(token);
-			// 현재날짜 가져오기
+			// 서버 내에서 현재날짜 가져오기
 			currentDate = LocalDateTime.now().toLocalDate();
 		}
 		
-		// DB에서 데이터 가져오기
+		// DB에서 데이터 가져오기 위한 로직 호출
 		Result result = service.result(email, currentDate);
-		System.out.println(result.getRchat());
+		// 값이 있을경우 body에 실어서 react로 값 보내기
 		if (result != null) {
 			return ResponseEntity.status(HttpStatus.OK).body(result);
 		} else {
@@ -54,15 +57,19 @@ public class ResultRestController {
 		}
 	}
 
-	// 수면 결과리스트 목록
+	// 수면 결과리스트 목록 요청
 	@GetMapping("/resultList")
 	public ResponseEntity<List<Result>> resultList(HttpServletRequest request) {
+		// react에서 Header에 실어서 요청한 값 가져오기
 		String authorizationHeader = request.getHeader("Authorization");
 
 		String email = null;
 		
+		// 넘어 온 값에서 토큰 처리하기
 		if (authorizationHeader != null & authorizationHeader.startsWith("Bearer")) {
+			// 토큰 값 앞에 "Bearer" 지우기
 			String token = authorizationHeader.substring(7);
+			// 넘어온 token에서 email값만 파싱하기
 			email = jwtUtil.getUserId(token);
 		}
 		
@@ -70,6 +77,7 @@ public class ResultRestController {
 		List<Result> resultList = service.resultList(email);
 		System.out.println(resultList.get(1).getRdate());
 		if(resultList != null) {
+			// 값이 있을경우 body에 실어서 react
 			return ResponseEntity.status(HttpStatus.OK).body(resultList); 
 		} else {
 			return ResponseEntity.status(HttpStatus.OK).body(null);
